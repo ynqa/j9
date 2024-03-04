5d55e87164d36c0fddea15f73a64cd6a1a68d454
ynqa <un.pensiero.vano@gmail.com>
2024-03-04 19:29:32 +0900

    docstring

--- j9/src/lib.rs
+++ j9/src/lib.rs
@@ -1,3 +1,8 @@
+//! `j9` is a Rust crate providing a high-level interface to the `jq` JSON processing library.
+//! It allows Rust applications to compile and execute `jq` scripts on JSON inputs, returning the
+//! processed results as Rust strings. This crate abstracts away the unsafe operations and direct
+//! FFI calls involved in using the `jq` library, offering a safe and idiomatic Rust API.
+
 use std::{
     ffi::{CStr, CString},
     os::raw::c_char,
@@ -20,6 +25,29 @@ unsafe fn get_string_from_c_char(x: *const c_char) -> Result<String> {
     Ok(CStr::from_ptr(x).to_str()?.to_string())
 }
 
+/// Provides a Rust interface to the `j9_sys` library, which is a Rust binding for the jq library.
+/// This module allows for compiling and running jq programs against specified input strings.
+///
+/// # Errors
+///
+/// This module can return errors in several cases, such as initialization failure, parsing errors,
+/// and compilation errors. These are encapsulated in the `Error` enum.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// use j9::run;
+///
+/// let program = ".[] | .key";
+/// let input = r#"[{"key": "value1"}, {"key": "value2"}]"#;
+/// let result = run(program, input);
+///
+/// assert!(result.is_ok());
+/// let values: Vec<String> = result.unwrap();
+/// assert_eq!(values, vec!["\"value1\"", "\"value2\""]);
+/// ```
 pub fn run(program: &str, input: &str) -> Result<Vec<String>> {
     let mut state = unsafe { jq_init() };
     if state.is_null() {
