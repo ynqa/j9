739f3a8cda69e08c3f01ad0b2589668ba44d0652
ynqa <un.pensiero.vano@gmail.com>
2024-11-14 01:03:15 +0900

    revert modification

--- j9-sys/build.rs
+++ j9-sys/build.rs
@@ -5,7 +5,10 @@ use std::{
     env,
     path::{Path, PathBuf},
     process::Command,
+    time::SystemTime,
 };
+
+use filetime::FileTime;
 
 fn check_installed(name: &str) -> anyhow::Result<()> {
     let check = Command::new(name).arg("--version").output();
@@ -37,6 +40,18 @@ fn main() -> anyhow::Result<()> {
 
     let out_dir = env::var("OUT_DIR").map(PathBuf::from)?;
     let src_dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("jq");
+
+    // Modify the timestamp of parser.c file
+    // to circumvent an error on building in Linux that goes something like:
+    //
+    //   clang: error: no such file or directory: 'src/parser.c'
+    //   clang: error: no input files
+    //   make[2]: *** [Makefile:1051: src/parser.lo] Error 1
+    //   make[1]: *** [Makefile:1188: install-recursive] Error 1
+    //   make: *** [Makefile:1709: install] Error 2
+    let now = FileTime::from(SystemTime::now());
+    let parser_src = src_dir.join("src/parser.c");
+    filetime::set_file_mtime(&parser_src, now)?;
 
     // See https://github.com/jqlang/jq/tree/jq-1.7.1?#instructions
     autotools::Config::new(&src_dir)
